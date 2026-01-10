1ed3eae09a73922aa7ee2e5c4e461b4c101cf9f1
Idowu Williams <kingidrogen@gmail.com>
2026-01-10 20:22:53 +0100

    Rust Ops

---
+++ loops/target/debug/deps/loops-f6985c36e67f120b.d
@@ -0,0 +1,5 @@
+/Users/ollawillie/projects/hello_world/rust-systems-fundamentals/loops/target/debug/deps/loops-f6985c36e67f120b.d: src/main.rs
+
+/Users/ollawillie/projects/hello_world/rust-systems-fundamentals/loops/target/debug/deps/loops-f6985c36e67f120b: src/main.rs
+
+src/main.rs:
---
+++ loops/target/debug/loops.d
@@ -0,0 +1,1 @@
+/Users/ollawillie/projects/hello_world/rust-systems-fundamentals/loops/target/debug/loops: /Users/ollawillie/projects/hello_world/rust-systems-fundamentals/loops/src/main.rs
