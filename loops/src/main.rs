171fa0a02d79ab0a624e4a738fde4d8c7632a726
Idowu Williams <kingidrogen@gmail.com>
2026-01-12 13:03:58 +0100

    Rust ops

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -10,4 +10,36 @@ fn main() {
     };
 
     println!("The result is {result}");
+
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+
+
+    let mut number = 5;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
 }
+
