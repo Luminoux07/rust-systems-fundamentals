127e3e905ffada6d19c6e8613788da063ca52491
Idowu Williams <kingidrogen@gmail.com>
2026-01-12 16:36:54 +0100

    Rust Ops

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -41,5 +41,25 @@ fn main() {
     }
 
     println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
+
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {element}");
+    }
+
+        for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
 }
 
