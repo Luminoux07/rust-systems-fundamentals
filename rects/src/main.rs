b4ea9a164f6ad08851316efd28c1e303e35802dd
Idowu Williams <kingidrogen@gmail.com>
2026-01-22 22:01:17 +0100

    Structs

--- rects/src/main.rs
+++ rects/src/main.rs
@@ -2,23 +2,23 @@
 struct Rectangle {
     width: u32,
     height: u32,
-    length: u32,
 }
 
 fn main() {
     let rect1 = Rectangle {
-        width: 100,
+        width: 40,
         height: 50,
-        length: 20,
     };
 
-    println!("The area is {} pixels", area(&rect1)
-        );
+    println!("The size is of {} pixels.",
+            area(&rect1));
+    println!("This is size {rect1:#?}");
+
+    dbg!(&rect1);
     
-    println!("The rectangle be like: {rect1:#?}")
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height / rectangle.length
+    rectangle.width * rectangle.height
 }
 
