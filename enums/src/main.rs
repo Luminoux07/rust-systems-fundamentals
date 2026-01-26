4565008e66ae20f945519ad9b8d8af799b1e91da
Idowu Williams <kingidrogen@gmail.com>
2026-01-26 18:21:12 +0100

    enum

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,16 +1,19 @@
 
 fn main() {
     #[derive(Debug)]
-    enum IpAddr {
-        V4(u8, u8, u8, u8),
-        V6(String),
+    enum Coin {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter,
     }
 
-    let home = IpAddr::V4(127, 0, 0, 1);
-
-    let loopback = IpAddr::V6(String::from("::1"));
-
-    println!("{home:#?}");
-    
-    println!("{loopback:#?}");
+    fn value_in_cents(coin: Coin) -> u8 {
+        match coin {
+            Coin::Penny => 1,
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter => 25,
+        }
+    }
 }
