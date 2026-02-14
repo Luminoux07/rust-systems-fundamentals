ea80c4373614b2930da6b24e5a49d0a9abcf878c
Idowu Williams <kingidrogen@gmail.com>
2026-02-14 23:49:16 +0100

    finacle

--- rects/src/main.rs
+++ rects/src/main.rs
@@ -3,13 +3,13 @@ use std::time::Duration;
 fn main() {
     trpl::block_on(async {
         trpl::spawn_task(async {
-            for i in 1..30 {
+            for i in 1..50 {
                 println!("hi number {i} from the first task!");
                 trpl::sleep(Duration::from_millis(500)).await;
             }
         });
 
-        for i in 1..30 {
+        for i in 1..50 {
             println!("hi number {i} from the second task!");
             trpl::sleep(Duration::from_millis(500)).await;
         }
